95f8c7a5bbd1db00130a9fa91be65aafe3b9b194
Sebastian <sebastian@yo.lk>
2011-01-06 20:22:34 +0100

    ActiveModel validation: added optional lookup support

--- lib/valvat/active_model.rb
+++ lib/valvat/active_model.rb
@@ -1,7 +1,16 @@
-class ValvatValidator < ::ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless Valvat::Syntax.validate(value)
-      record.errors.add(attribute, options[:message])
+module ActiveModel
+  module Validations
+    class ValvatValidator < ::ActiveModel::EachValidator
+      def validate_each(record, attribute, value)
+        is_valid = Valvat::Syntax.validate(value)
+        
+        if is_valid && options[:lookup]
+          is_valid = Valvat::Lookup.validate(value)
+          is_valid.nil? && is_valid = (options[:lookup] != :fail_if_down)
+        end
+
+        record.errors.add(attribute, options[:message]) unless is_valid
+      end
     end
   end
 end
--- spec/valvat/active_model_spec.rb
+++ spec/valvat/active_model_spec.rb
@@ -4,6 +4,14 @@ class Invoice < ModelBase
   validates :vat_number, :valvat => true
 end
 
+class InvoiceWithLookup < ModelBase  
+  validates :vat_number, :valvat => {:lookup => true}
+end
+
+class InvoiceWithLookupAndFailIfDown < ModelBase  
+  validates :vat_number, :valvat => {:lookup => :fail_if_down}
+end
+
 describe Invoice do
   context "with valid vat number" do
     before do
@@ -11,7 +19,7 @@ describe Invoice do
     end
     
     it "should be valid" do
-      Invoice.new(:vat_number => "DE345889003").should be_valid
+      Invoice.new(:vat_number => "DE123").should be_valid
     end
   end
   
@@ -21,7 +29,55 @@ describe Invoice do
     end
     
     it "should be valid" do
-      Invoice.new(:vat_number => "DE345889003").should_not be_valid
+      Invoice.new(:vat_number => "DE123").should_not be_valid
+    end
+  end
+end
+
+describe InvoiceWithLookup do
+  context "with valid but not existing vat number" do
+    before do
+      Valvat::Syntax.stub(:validate => true)
+      Valvat::Lookup.stub(:validate => false)
+    end
+    
+    it "should be valid" do
+      InvoiceWithLookup.new(:vat_number => "DE123").should_not be_valid
+    end
+  end
+  
+  context "with valid and existing vat number" do
+    before do
+      Valvat::Syntax.stub(:validate => true)
+      Valvat::Lookup.stub(:validate => true)
+    end
+    
+    it "should be valid" do
+      InvoiceWithLookup.new(:vat_number => "DE123").should be_valid
+    end
+  end
+  
+  context "with valid and VIES country service down" do
+    before do
+      Valvat::Syntax.stub(:validate => true)
+      Valvat::Lookup.stub(:validate => nil)
+    end
+    
+    it "should be valid" do
+      InvoiceWithLookup.new(:vat_number => "DE123").should be_valid
+    end
+  end
+end
+
+describe InvoiceWithLookupAndFailIfDown do
+  context "with valid and VIES country service down" do
+    before do
+      Valvat::Syntax.stub(:validate => true)
+      Valvat::Lookup.stub(:validate => nil)
+    end
+    
+    it "should be valid" do
+      InvoiceWithLookupAndFailIfDown.new(:vat_number => "DE123").should_not be_valid
     end
   end
 end
