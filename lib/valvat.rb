ff12df4534c643d32b19ecd66a44a794872829ef
Vincent De Snerck <thespobo@gmail.com>
2011-08-31 14:03:37 +0200

    Added normalization to initialize

--- lib/valvat.rb
+++ lib/valvat.rb
@@ -1,36 +1,36 @@
 class Valvat
   def initialize(raw)
-    @raw = raw || ""
+    @raw = Valvat::Utils.normalize(raw) || ""
     @vat_country_code, @to_s_wo_country = to_a
   end
-  
+
   attr_reader :raw, :vat_country_code, :to_s_wo_country
-  
+
   def valid?
     Valvat::Syntax.validate(self)
   end
-  
+
   def exists?
     Valvat::Lookup.validate(self)
   end
   alias_method :exist?, :exists?
-  
+
   def iso_country_code
     Valvat::Utils.vat_country_to_iso_country(vat_country_code)
   end
-  
+
   def european?
     Valvat::Utils::EU_COUNTRIES.include?(iso_country_code)
   end
-  
+
   def to_a
     Valvat::Utils.split(raw)
   end
-  
+
   def to_s
     raw
   end
-  
+
   def inspect
     "#<Valvat #{[raw, iso_country_code].compact.join(" ")}>"
   end
