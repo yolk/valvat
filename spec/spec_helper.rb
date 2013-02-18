244f2ce22211133be1e21082f6ed3730afc1ecf6
Sebastian <sebastian@yo.lk>
2013-02-19 00:28:51 +0100

    rspec config: backtrace_clean_patterns

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,7 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
+  config.backtrace_clean_patterns = [/rspec\/(core|expectations)/]
 end
 
 class ModelBase
