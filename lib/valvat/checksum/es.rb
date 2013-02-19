e2c738d8a31d7b404b7fb05caefc40fd78b94ebb
Sebastian <sebastian@yo.lk>
2013-02-19 19:11:51 +0100

    Checksum::ES small refactorings

--- lib/valvat/checksum/es.rb
+++ lib/valvat/checksum/es.rb
@@ -3,21 +3,15 @@ require 'valvat/checksum'
 class Valvat
   module Checksum
     class ES < Base
-      def check_digit
-        natural_person? || natural_foreign_person? ?
-          check_digit_natural_person :
-          check_digit_legal_person
-      end
+      NATURAL_PERSON_CHARS       = %w(T R W A G M Y F P D X B N J Z S Q V H L C K E)
+      LEGAL_FOREIGN_PERSON_CHARS = [false] + %w(A B C D E F G H I J)
 
-      NATURAL_PERSON_CHARS = %w(T R W A G M Y F P D X B N J Z S Q V H L C K E)
-      LEGAL_FOREIGN_PERSON_CHARS = %w(XXX A B C D E F G H I J)
+      def check_digit
+        natural_person? ? check_digit_natural_person : check_digit_legal_person
+      end
 
       def check_digit_natural_person
         NATURAL_PERSON_CHARS[figures_str.to_i.modulo(23)]
-      end
-
-      def given_check_digit
-        natural_person? || legal_foreign_person? || natural_foreign_person? ? str_wo_country[-1] : super
       end
 
       def check_digit_legal_person
@@ -27,24 +21,22 @@ class Valvat
         legal_foreign_person? ? LEGAL_FOREIGN_PERSON_CHARS[chk] : chk
       end
 
+      def given_check_digit
+        natural_person? || legal_foreign_person? ? str_wo_country[-1] : super
+      end
+
       def str_wo_country
-        if natural_person?
-          super
-        else
-          super[1..-1]
-        end
+        str = super
+        str[0] =~ /\d/ ? str : str[1..-1]
       end
 
       def natural_person?
-        !!(vat.to_s_wo_country =~ /\A[\d]{8}[ABCDEFGHJKLMNPQRSTVWXYZ]\Z/)
+        !!(vat.to_s_wo_country =~ /\A[\d]{8}[ABCDEFGHJKLMNPQRSTVWXYZ]\Z/) ||
+        !!(vat.to_s_wo_country =~ /\A[KLMX][\d]{7}[ABCDEFGHJKLMNPQRSTVWXYZ]\Z/)
       end
 
       def legal_foreign_person?
         !!(vat.to_s_wo_country =~ /\A[NPQRSW][\d]{7}[ABCDEFGHIJ]\Z/)
-      end
-
-      def natural_foreign_person?
-        !!(vat.to_s_wo_country =~ /\A[KLMX][\d]{7}[ABCDEFGHJKLMNPQRSTVWXYZ]\Z/)
       end
     end
   end
