41831b92b1f2f6edf312672a046a875e7db1343d
Sebastian <sebastian@yo.lk>
2013-02-19 19:20:55 +0100

    Spec: Added support for external data file of valid vats

--- spec/valvat/checksum_spec.rb
+++ spec/valvat/checksum_spec.rb
@@ -1,9 +1,21 @@
 require 'spec_helper'
 
+begin
+  require File.dirname(__FILE__) + '/../../tmp/valid_vat_numbers.rb'
+rescue LoadError
+  VALID_VAT_NUMBERS = []
+end
+
 describe Valvat::Checksum do
   describe "#validate" do
     it "returns true on vat number with unknown checksum algorithm" do
       Valvat::Checksum.validate("FR99123543267").should eql(true)
+    end
+
+    VALID_VAT_NUMBERS.each do |valid_vat|
+      it "returns true on valid vat number #{valid_vat}" do
+        Valvat::Checksum.validate(valid_vat).should eql(true)
+      end
     end
   end
 end
