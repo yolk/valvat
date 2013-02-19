0e3f1e3b9b0c070f2ac34f227c72675bc0e0e89c
Sebastian <sebastian@yo.lk>
2013-02-19 03:07:46 +0100

    Checksum: Added AT support

--- lib/valvat/checksum.rb
+++ lib/valvat/checksum.rb
@@ -53,6 +53,19 @@ class Valvat
       end
     end
 
+    class AT < Base
+      def check_digit
+        chk = 96 - figures.reverse.each_with_index.map do |fig, i|
+          (fig*(i.modulo(2) == 0 ? 1 : 2)).to_s.split("").inject(0) { |sum, n| sum + n.to_i }
+        end.inject(:+)
+        chk.to_s[-1].to_i
+      end
+
+      def str_wo_country
+        super[1..-1]
+      end
+    end
+
     class BE < Base
       check_digit_length 2
 
--- spec/valvat/checksum_spec.rb
+++ spec/valvat/checksum_spec.rb
@@ -135,5 +135,19 @@ describe Valvat::Checksum do
         end
       end
     end
+
+    context "validates AT" do
+      %w(ATU13585627 ATU66059506 ATU42403001).each do |valid_vat|
+        it "returns true on valid vat #{valid_vat}" do
+          subject.validate(valid_vat).should eql(true)
+        end
+
+        invalid_vat = "#{valid_vat[0..-4]}#{valid_vat[-1]}#{valid_vat[-2]}#{valid_vat[-3]}"
+
+        it "returns false on invalid vat #{invalid_vat} #{valid_vat}" do
+          subject.validate(invalid_vat).should eql(false)
+        end
+      end
+    end
   end
 end
