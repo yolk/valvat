32d35da711007f5414507428d375772a62ebe7fb
Sebastian <sebastian@yo.lk>
2013-02-19 02:49:31 +0100

    Checksum: Added IT support

--- spec/valvat/checksum_spec.rb
+++ spec/valvat/checksum_spec.rb
@@ -78,11 +78,33 @@ describe Valvat::Checksum do
           subject.validate(valid_vat).should eql(true)
         end
 
+        invalid_vat = "#{valid_vat[0..-4]}#{valid_vat[-2]}#{valid_vat[-3]}#{valid_vat[-1]}"
+
+        it "returns false on invalid vat #{invalid_vat} #{valid_vat}" do
+          subject.validate(invalid_vat).should eql(false)
+        end
+      end
+    end
+
+    context "validates IT" do
+      %w(IT12345670785 IT01897810162 IT00197200132).each do |valid_vat|
+        it "returns true on valid vat #{valid_vat}" do
+          subject.validate(valid_vat).should eql(true)
+        end
+
         invalid_vat = "#{valid_vat[0..-3]}#{valid_vat[-1]}#{valid_vat[-2]}"
 
         it "returns false on invalid vat #{invalid_vat} #{valid_vat}" do
           subject.validate(invalid_vat).should eql(false)
         end
+      end
+
+      it "returns false on invalid special case vat IT12345671783" do
+        subject.validate("IT12345671783").should eql(false)
+      end
+
+      it "returns false on invalid special case vat IT00000000133" do
+        subject.validate("IT00000000133").should eql(false)
       end
     end
   end
