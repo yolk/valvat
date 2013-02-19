5a90af5c54edc0d2a6973fd455993b0702ef86d2
Sebastian <sebastian@yo.lk>
2013-02-19 02:03:18 +0100

    Checksum: Moved logic from lambdas to classes

--- lib/valvat/checksum.rb
+++ lib/valvat/checksum.rb
@@ -2,53 +2,106 @@ require 'valvat'
 
 class Valvat
   module Checksum
-    ALGORITHMS = {
-      "BE" => lambda{ |vat|
-        str = vat.to_s_wo_country
-        (97 - str[0..-3].to_i.modulo(97)) == str[-2..-1].to_i
-      },
-      "DE" => lambda{ |vat|
-        m, n = 10, 11
-        prod, sum = m, 0
-        figures = vat.to_s_wo_country.split("").map(&:to_i)
-        figures[0..7].each do |fig|
-          sum = (prod + fig).modulo(m)
-          sum = m if sum == 0
-          prod = (2*sum).modulo(n)
-        end
-        chk = n - prod
-        chk = 0 if chk == 10
-        chk == figures.last
-      },
-      "DK" => lambda{ |vat|
-        weight = [2, 7, 6, 5, 4, 3, 2, 1]
-        vat.to_s_wo_country.split("").map do |fig|
-          fig.to_i * weight.shift
-        end.inject(:+).modulo(11) == 0
-      },
-      "FI" => lambda{ |vat|
-        weight = [7, 9, 10, 5, 8, 4, 2]
-        r = 11 - vat.to_s_wo_country.split("")[0..-2].map do |fig|
-          fig.to_i * weight.shift
-        end.inject(:+).modulo(11)
-        return false if r == 10
-        r = 0 if r == 11
-        r == vat.to_s_wo_country[-1].to_i
-      },
-      "GR" => lambda{ |vat|
-        figures = []
-        vat.to_s_wo_country.split("")[0..-2].reverse.each_with_index do |fig, i|
-          figures << fig.to_i*(2**(i+1))
-        end
-        r = figures.inject(:+).modulo(11)
-        (r > 9 ? 0 : r) == vat.to_s_wo_country[-1].to_i
-      },
-    }
+    ALGORITHMS = {}
 
     def self.validate(vat)
       vat = Valvat(vat)
       algo = ALGORITHMS[vat.iso_country_code]
-      !!(algo.nil? || algo.call(vat))
+      !!(algo.nil? || algo.new(vat).validate)
+    end
+
+    class Base
+      def self.inherited(klass)
+        ALGORITHMS[klass.name.split(/::/).last] = klass
+      end
+
+      attr_reader :vat
+
+      def self.check_digit_length(len=nil)
+        @check_digit_length = len if len
+        @check_digit_length || 1
+      end
+
+      def initialize(vat)
+        @vat = vat
+      end
+
+      def validate
+        check_digit == given_check_digit
+      end
+
+      private
+
+      def given_check_digit
+        vat.to_s_wo_country[-self.class.check_digit_length..-1].to_i
+      end
+
+      def figures_str
+        vat.to_s_wo_country[0..-(self.class.check_digit_length+1)]
+      end
+
+      def figures
+        figures_str.split("").map(&:to_i)
+      end
+    end
+
+    class BE < Base
+      check_digit_length 2
+
+      def check_digit
+        97 - figures_str.to_i.modulo(97)
+      end
+    end
+
+    class DE < Base
+      M = 10
+      N = 11
+
+      def check_digit
+        prod = M
+        figures.each do |fig|
+          sum = (prod + fig).modulo(M)
+          sum = M if sum == 0
+          prod = (2*sum).modulo(N)
+        end
+        chk = N - prod
+        chk == 10 ? 0 : chk
+      end
+    end
+
+    class DK < Base
+      check_digit_length 0
+
+      def given_check_digit
+        0
+      end
+
+      def check_digit
+        weight = [2, 7, 6, 5, 4, 3, 2, 1]
+        figures.map do |fig|
+          fig * weight.shift
+        end.inject(:+).modulo(11)
+      end
+    end
+
+    class FI < Base
+      def check_digit
+        weight = [7, 9, 10, 5, 8, 4, 2]
+        chk = 11 - figures.map do |fig|
+          fig * weight.shift
+        end.inject(:+).modulo(11)
+        return false if chk == 10
+        chk == 11 ? 0 : chk
+      end
+    end
+
+    class GR < Base
+      def check_digit
+        chk = figures.reverse.each_with_index.map do |fig, i|
+          fig*(2**(i+1))
+        end.inject(:+).modulo(11)
+        chk > 9 ? 0 : chk
+      end
     end
   end
 end
