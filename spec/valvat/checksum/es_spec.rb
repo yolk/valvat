d202249166f17d0a6fdb9829d7f093c98fc2ea99
David Escala <descala@ingent.net>
2015-03-12 14:54:14 +0100

    Adds support for spanish NIE Y and Z

--- spec/valvat/checksum/es_spec.rb
+++ spec/valvat/checksum/es_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Valvat::Checksum::ES do
-  %w(ESA13585625 ESB83871236 ESE54507058 ES25139013J ESQ1518001A ESQ5018001G ESX4942978W ESX7676464F ESB10317980).each do |valid_vat|
+  %w(ESA13585625 ESB83871236 ESE54507058 ES25139013J ESQ1518001A ESQ5018001G ESX4942978W ESX7676464F ESB10317980 ESY3860557K ESY2207765D).each do |valid_vat|
     it "returns true on valid vat #{valid_vat}" do
       expect(Valvat::Checksum.validate(valid_vat)).to eql(true)
     end
