b9674acee8f4ed0d790cd838b661e6d64ba24b24
Nikola Nenkov <ndnenkov@gmail.com>
2017-05-10 17:16:32 +0300

    Add Malta checksum validation
    Source:
    http://85.81.229.78/systems/DKVIES/-%20Arkiv/Algoritme%E6ndringer/VIES-VAT%20Validation%20Routines-v15.0.doc

---
+++ lib/valvat/checksum/mt.rb
@@ -0,0 +1,16 @@
+class Valvat
+  module Checksum
+    class MT < Base
+      check_digit_length 2
+
+      def check_digit
+        multipliers = [9, 8, 7, 6, 4, 3]
+
+        sum = sum_figures_by { |digit, index| digit * multipliers[index] }
+
+        supposed_checksum = 37 - (sum % 37)
+        supposed_checksum.zero? ? 37 : supposed_checksum
+      end
+    end
+  end
+end
