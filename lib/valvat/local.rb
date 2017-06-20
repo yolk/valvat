ef5e768366737cd4a74202c8c1bc1519b11bb71c
Sebastian <sebastian@yo.lk>
2017-06-20 13:49:26 +0200

    Allow to require all functionality except lookup with 'valvat/local'
    More modular. Installed savon gem is still needed, but won't be loaded.
    Normal loading interface is not changed.
    Just require 'valvat/local' to load valvat without lookup (savon won't be loaded). In your Gemfile add
    "gem 'valvat', :require => 'valvat/local'".
    [#36]

---
+++ lib/valvat/local.rb
@@ -0,0 +1,49 @@
+class Valvat
+  def initialize(raw)
+    @raw = Valvat::Utils.normalize(raw || "")
+    @vat_country_code, @to_s_wo_country = to_a
+  end
+
+  attr_reader :raw, :vat_country_code, :to_s_wo_country
+
+  def blank?
+    raw.nil? || raw.strip == ""
+  end
+
+  def valid?
+    Valvat::Syntax.validate(self)
+  end
+
+  def valid_checksum?
+    Valvat::Checksum.validate(self)
+  end
+
+  def iso_country_code
+    Valvat::Utils.vat_country_to_iso_country(vat_country_code)
+  end
+
+  def european?
+    Valvat::Utils::EU_COUNTRIES.include?(iso_country_code)
+  end
+
+  def to_a
+    Valvat::Utils.split(raw)
+  end
+
+  def to_s
+    raw
+  end
+
+  def inspect
+    "#<Valvat #{[raw, iso_country_code].compact.join(" ")}>"
+  end
+end
+
+def Valvat(vat)
+  vat.is_a?(Valvat) ? vat : Valvat.new(vat)
+end
+
+require 'valvat/utils'
+require 'valvat/syntax'
+require 'valvat/checksum'
+require 'valvat/version'
