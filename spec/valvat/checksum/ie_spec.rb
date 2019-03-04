42a300e422b9fd57ee1e801e515e8afde49da340
Lluís Gili <lluis@ingent.net>
2019-03-04 13:35:37 +0100

    new IE vat number format, with 2 letters

--- spec/valvat/checksum/ie_spec.rb
+++ spec/valvat/checksum/ie_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe Valvat::Checksum::IE do
-  %w(IE8473625E IE0123459N IE9B12345N).each do |valid_vat|
+  %w(IE8473625E IE0123459N IE9B12345N IE1113571MH IE2973912UH IE2974611LH
+  IE2974901UH IE3200115LH IE3206791MH IE3208913KH IE3214048CH).each do |valid_vat|
     it "returns true on valid vat #{valid_vat}" do
-      expect(Valvat::Checksum.validate(valid_vat)).to eql(true)
+      expect(Valvat::Checksum.validate(valid_vat)).to eql(true), valid_vat
     end
 
     invalid_vat = "#{valid_vat[0..-4]}#{valid_vat[-2]}#{valid_vat[-3]}#{valid_vat[-1]}"
