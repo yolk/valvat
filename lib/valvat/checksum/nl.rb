3492e9122eb13e85251e8a66e5eea77d064a47a8
Yvonne <yvonne@moneybird.com>
2019-12-06 15:12:26 +0100

    validate new dutch 2020 vat identification number

--- lib/valvat/checksum/nl.rb
+++ lib/valvat/checksum/nl.rb
@@ -1,6 +1,11 @@
 class Valvat
   module Checksum
     class NL < Base
+      def validate
+        vat.to_s.gsub(/[A-Z]/) { |let| (let.ord - 55).to_s }.to_i % 97 == 1 ||
+        super
+      end
+
       def check_digit
         sum_figures_by do |fig, i|
           fig*(i+2)
