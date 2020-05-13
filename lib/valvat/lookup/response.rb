f905b86548fb0c7687061e77c82aeea34ab8a790
Sebastian <sebastian@yo.lk>
2020-05-13 15:11:50 +0200

    Refactored Lookup::Response

--- lib/valvat/lookup/response.rb
+++ lib/valvat/lookup/response.rb
@@ -17,13 +17,20 @@ class Valvat
       private
 
       def self.cleanup_hash(hash)
-        (hash[:check_vat_approx_response] || hash[:check_vat_response] || {}).inject({}) do |hash, kv|
-          key, value = kv
-          unless key == :"@xmlns"
-            hash[key.to_s.sub(/\Atrader_/, "").to_sym] = (value == "---" ? nil : value)
-          end
+        (hash[:check_vat_approx_response] || hash[:check_vat_response] || {}).inject({}) do |hash, (key, value)|
+          hash[cleanup_key(key)] = cleanup_value(value) unless key == :"@xmlns"
           hash
         end
+      end
+
+      TRADER_PREFIX = /\Atrader_/
+
+      def self.cleanup_key(key)
+        key.to_s.sub(TRADER_PREFIX, "").to_sym
+      end
+
+      def self.cleanup_value(value)
+        value == "---" ? nil : value
       end
     end
   end
