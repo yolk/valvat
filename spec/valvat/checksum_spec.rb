a437a6f1180bf835b80cd7cccf2bbca649450f40
Sebastian <sebastian@yo.lk>
2021-01-20 17:50:30 +0100

    Checksum specs: Added some false positives (only DE)

--- spec/valvat/checksum_spec.rb
+++ spec/valvat/checksum_spec.rb
@@ -8,6 +8,13 @@ rescue LoadError
   VALID_VAT_NUMBERS = [].freeze
 end
 
+# Syntax is correct, but checksum invalid
+INVALID_VAT_NUMBERS = %w[
+  DE002768611 DE161216774 DE192970834 DE223361232 DE226095231
+  DE227411313 DE230928917 DE232123248 DE267748405 DE275615509
+  DE311927825 DE329214395 DE815842425 DE821530465 DE999999999
+].freeze
+
 describe Valvat::Checksum do
   describe '#validate' do
     it 'returns true on VAT number with unknown checksum algorithm' do
@@ -21,6 +28,12 @@ describe Valvat::Checksum do
     VALID_VAT_NUMBERS.each do |valid_vat|
       it "returns true on valid VAT number #{valid_vat}" do
         expect(described_class.validate(valid_vat)).to be(true)
+      end
+    end
+
+    INVALID_VAT_NUMBERS.each do |invalid_vat|
+      it "returns false on invalid VAT number #{invalid_vat}" do
+        expect(described_class.validate(invalid_vat)).to be(false)
       end
     end
   end
