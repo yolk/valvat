8ee522efd49ddce2217ee4481478e3d9f4e28257
Sebastian <sebastian@yo.lk>
2022-01-26 16:21:21 +0100

    Fix I18n.translate keyword arguments error

--- lib/active_model/validations/valvat_validator.rb
+++ lib/active_model/validations/valvat_validator.rb
@@ -74,7 +74,7 @@ module ActiveModel
                           message: options[:message],
                           country_adjective: I18n.t(
                             :"valvat.country_adjectives.#{iso_country_code.downcase}",
-                            default: [:"valvat.country_adjectives.eu", 'european']
+                            :default => [:"valvat.country_adjectives.eu", 'european']
                           ))
       end
 
