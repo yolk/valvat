293fed885392efaad72ae641628b6c098c3b5972
Thomas Scalise <tscalise@ingent.net>
2022-09-12 10:19:18 +0200

    Fix for Spain natural person VAT Validation: If the VAT starts with [KLMXYZ\d], we should always validate as a natural person. In some cases, we were applying Legal Person rules, causing invalid NIF's to be validated.

--- lib/valvat/checksum/es.rb
+++ lib/valvat/checksum/es.rb
@@ -4,7 +4,7 @@ class Valvat
   module Checksum
     class ES < Base
       NATURAL_PERSON_CHARS       = %w[T R W A G M Y F P D X B N J Z S Q V H L C K E].freeze
-      NATURAL_PERSON_EXP         = /\A(\d{8}[ABCDEFGHJKLMNPQRSTVWXYZ]|[KLMXYZ]\d{7}[ABCDEFGHJKLMNPQRSTVWXYZ])\Z/.freeze
+      NATURAL_PERSON_EXP         = /\A([KLMXYZ\d])/.freeze
       LEGAL_PERSON_CHARS = [false] + %w[A B C D E F G H I J]
       LEGAL_PERSON_EXP   = /\A[NPQRSW]\d{7}[ABCDEFGHIJ]\Z/.freeze
       NIE_DIGIT_BY_LETTER = %w[X Y Z].freeze
