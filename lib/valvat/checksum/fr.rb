5c8ccccc11e63f9e838363dd8113af0528bc90d0
Sebastian <sebastian@yo.lk>
2023-04-17 12:03:14 +0200

    Checksum::FR: Rewrote for ruby 2.5 compatibility + syntactic rearrangements

--- lib/valvat/checksum/fr.rb
+++ lib/valvat/checksum/fr.rb
@@ -3,22 +3,18 @@
 class Valvat
   module Checksum
     class FR < Base
-
       # the valid characters for the first two digits (O and I are missing)
       ALPHABET = '0123456789ABCDEFGHJKLMNPQRSTUVWXYZ'
 
       def validate
-        if str_wo_country[0..1] =~ /^\d+$/
-          super
-        else
-          check =
-            if str_wo_country[0] =~ /\d/
-              ALPHABET.index(str_wo_country[0]) * 24 + ALPHABET.index(str_wo_country[1]) - 10
-            else
-              ALPHABET.index(str_wo_country[0]) * 34 + ALPHABET.index(str_wo_country[1]) - 100
-            end
-          (str_wo_country[2..].to_i + 1 + check / 11) % 11 == check % 11
-        end
+        return super if str_wo_country[0..1] =~ /^\d+$/
+
+        numeric_start = str_wo_country[0] =~ /\d/
+
+        check = (ALPHABET.index(str_wo_country[0]) * (numeric_start ? 24 : 34)) +
+                ALPHABET.index(str_wo_country[1]) - (numeric_start ? 10 : 100)
+
+        (str_wo_country[2..-1].to_i + 1 + (check / 11)) % 11 == check % 11
       end
 
       def check_digit
