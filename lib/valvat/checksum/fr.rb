9ba2d7f3d94e42f4374d3df6545c9cbe628bcb0f
Sebastian <sebastian@yo.lk>
2023-04-19 12:40:30 +0200

    Checksum:FR: Some refactorings

--- lib/valvat/checksum/fr.rb
+++ lib/valvat/checksum/fr.rb
@@ -5,17 +5,17 @@ class Valvat
     class FR < Base
       # the valid characters for the first two digits (O and I are missing)
       ALPHABET = '0123456789ABCDEFGHJKLMNPQRSTUVWXYZ'
+      NUMERIC = /^\d+$/.freeze
 
       def validate
-        return super if str_wo_country[0..1] =~ /^\d+$/
+        return super if str_wo_country[0..1] =~ NUMERIC
 
-        numeric_start = str_wo_country[0] =~ /\d/
-
-        check = (ALPHABET.index(str_wo_country[0]) * (numeric_start ? 24 : 34)) +
-                ALPHABET.index(str_wo_country[1]) - (numeric_start ? 10 : 100)
+        check = alt_check_digit
 
         (str_wo_country[2..].to_i + 1 + (check / 11)) % 11 == check % 11
       end
+
+      private
 
       def check_digit
         siren = str_wo_country[2..].to_i
@@ -24,6 +24,13 @@ class Valvat
 
       def given_check_digit
         str_wo_country[0..1].to_i
+      end
+
+      def alt_check_digit
+        first_is_numeric = str_wo_country[0] =~ NUMERIC
+
+        (ALPHABET.index(str_wo_country[0]) * (first_is_numeric ? 24 : 34)) +
+          ALPHABET.index(str_wo_country[1]) - (first_is_numeric ? 10 : 100)
       end
     end
   end
